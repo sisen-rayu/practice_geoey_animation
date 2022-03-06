a25a0c7f58f29ab65aec43ed4d02279c73dd92f1
chihiro <chihiro.tanigawa@accenture.com>
2022-03-07 02:32:18 +0900

    geoey

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
-const $cursor = document.querySelector('#cursor');
-document.addEventListener('mousemove', (e) => {
-  let x = e.clientX;
-  let y = e.clientY;
-  $cursor.style.left = x + 'px';
-  $cursor.style.top = y + 'px';
-});
+let container = document.querySelector('#container');
+let count = 40;
+for (let i = 0; i < 40; i++){
+  let $gooeyBox = document.createElement('div')
+  $gooeyBox.className = 'box';
+  container.appendChild($gooeyBox);
+}
+
+setInterval(() => {
+  let $gooey = document.getElementsByClassName('box');
+for (let i = 0; i < $gooey.length; i++){
+  $gooey[i].style.left = Math.floor(Math.random()*90) + 'vw';
+  $gooey[i].style.top = Math.floor(Math.random()*80) + 'vh';
+}
+},2000);
+
